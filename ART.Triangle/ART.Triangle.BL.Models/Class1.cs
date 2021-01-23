59d6d4fbac99af87b4a8b6ee3018909b8d8690e3
Adam Tamminga <500158560@fvtc.edu>
2021-01-23 14:05:04 -0600

    Added the BL.Models Triangle class

--- ART.Triangle/ART.Triangle.BL.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ART.Triangle.BL.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ ART.Triangle/ART.Triangle.BL.Models/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ART.Triangle.BL.Models
+{
+    public class Triangle
+    {
+        public Guid Id { get; set; }
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime ChangeDate { get; set; }
+
+
+    }
+}
